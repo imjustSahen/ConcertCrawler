6b4fcaf2814fc2839fc2aa0e1e3a168608fa82dc
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2022-12-07 18:36:25 -0800

    added to contact.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'black': '#000000',
+      'white': "#FFFFFF",
+      'gray': {
+      50: '#f9fafb',
+      100: '#f3f4f6',
+      200: '#e5e7eb',
+      300: '#d1d5db',
+      400: '#9ca3af',
+      500: '#6b7280',
+      600: '#4b5563',
+      700: '#374151',
+      800: '#1f2937',
+      900: '#111827'
+      }
+    },
+
     extend: {},
   },
   plugins: [],
